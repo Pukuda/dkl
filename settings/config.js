f532c8f758538016f5d38baab16d30af9b83ecc6
pu pi <bambanglutt@gmail.com>
2022-07-20 10:19:49 +0700

    SelfBot

---
+++ settings/config.js
@@ -0,0 +1,49 @@
+const fs = require('fs')
+const chalk = require('chalk')
+
+// Website Api
+global.APIs = {
+beta: 'https://betabotz-api.herokuapp.com',
+}
+
+// Apikey Website Api
+global.APIKeys = {
+'https://betabotz-api.herokuapp.com': 'BetaBotz',
+}
+
+// Other
+global.owner = ['62882005687229']
+global.premium = ['62882005687229']
+global.botname = '𝚃𝙰𝚂𝚈𝙰𝙱𝙾𝚃'
+global.ownername = '𝙵'
+global.packname = '© 𝚃𝙰𝚂𝚈𝙰𝙱𝙾𝚃'
+global.webme = 'https://betabotz-api.herokuapp.com' 
+global.author = '𝘽𝙔 𝙁'
+global.sessionName = 'F'
+global.prefa = ['','!','.','🐦','🐤','🗿']
+global.sp = '⭔'
+global.mess = {
+success: '✓ Success',
+admin: 'Fitur Khusus Admin Group!',
+botAdmin: 'Bot Harus Menjadi Admin Terlebih Dahulu!',
+owner: 'Fitur Khusus Owner Bot',
+group: 'Fitur Digunakan Hanya Untuk Group!',
+private: 'Fitur Digunakan Hanya Untuk Private Chat!',
+bot: 'Fitur Khusus Pengguna Nomor Bot',
+wait: 'Loading...',
+endLimit: 'Limit Harian Anda Telah Habis, Limit Akan Direset Setiap Jam 12',
+}
+global.limitawal = {
+premium: "Infinity",
+free: 20
+}
+global.thumb = fs.readFileSync('./media/thumb.jpg')
+global.visoka = { url: 'https://telegra.ph/file/15209657f9d4f59c7ca1e.mp4' }
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+fs.unwatchFile(file)
+console.log(chalk.redBright(`Update'${__filename}'`))
+delete require.cache[file]
+require(file)
+})
